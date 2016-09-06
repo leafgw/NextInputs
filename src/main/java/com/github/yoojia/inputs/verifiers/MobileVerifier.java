6dbf9fd3d96646a1386c47506a40192ca52fe298
YOOJIA.CHEN <yoojiachen@gmail.com>
2016-09-06 16:47:19 +0800

    Update Mobile verifier

--- src/main/java/com/github/yoojia/inputs/verifiers/MobileVerifier.java
+++ src/main/java/com/github/yoojia/inputs/verifiers/MobileVerifier.java
@@ -10,8 +10,15 @@ import static com.github.yoojia.inputs.Texts.regexMatch;
  * @since 1.1
  */
 public class MobileVerifier extends EmptyableVerifier {
+
+    /**
+     * 中国移动：134,135,136,137,138,139,147,150,151,152,157,158,158,178,182,183,184,187,188
+     * 中国联通：130,131,132,145,155,156,175,176,185,186
+     * 中国电信：133,134,149,153,173,177,180,181,189
+     * 虚拟运营商：170,171
+     */
     @Override
     public boolean performTestNotEmpty(String notEmptyInput) throws Exception {
-        return regexMatch(notEmptyInput, "^(\\+?\\d{2}-?)?(1[0-9])\\d{9}$");
+        return regexMatch(notEmptyInput, "^(\\+?\\d{2}-?)?(1[34578])\\d{9}$");
     }
 }
